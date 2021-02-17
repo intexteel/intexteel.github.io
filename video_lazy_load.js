725088c90ff7b20d174c1c2c05c552e291b198c8
Nicusor Chiciuc <nicuchiciuc@gmail.com>
2021-02-17 17:57:54 +0200

    Applied prettier to video_lazy_load

--- video_lazy_load.js
+++ video_lazy_load.js
@@ -1,69 +1,64 @@
 var videos = document.getElementsByTagName("iframe");
-for(var i=0; i<videos.length; i++)
-{
+for (var i = 0; i < videos.length; i++) {
   var video = videos[i];
-  video.id = "video-"+i;
-  video.setAttribute('data-src', video.src + "&version=3&enablejsapi=1&html5=1");
-  video.src = '';
+  video.id = "video-" + i;
+  video.setAttribute(
+    "data-src",
+    video.src + "&version=3&enablejsapi=1&html5=1"
+  );
+  video.src = "";
 }
 
 // Inject YouTube API script
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // global variable for the player
-var players = {}
-var playersLoaded = {}
-var playersVisible = {}
+var players = {};
+var playersLoaded = {};
+var playersVisible = {};
 console.log("videos found: " + videos.length);
 
-players['test'] = 'testp';
+players["test"] = "testp";
 console.log(players);
 
 var readyFrames = [];
-var observer = new IntersectionObserver(function(entries) {
-
-  // isIntersecting is true when element and viewport are overlapping
-  // isIntersecting is false when element and viewport don't overlap
+var observer = new IntersectionObserver(
+  function (entries) {
+    // isIntersecting is true when element and viewport are overlapping
+    // isIntersecting is false when element and viewport don't overlap
     console.log("Callback");
 
-console.log(entries);
-    for(var i=0; i<entries.length; i++)
-    {
+    console.log(entries);
+    for (var i = 0; i < entries.length; i++) {
       var iframe = entries[i].target;
       console.log(entries);
       playersVisible[iframe.id] = entries[i].isIntersecting;
 
-      if(entries[i].isIntersecting == true)
-      {
-        if(!playersLoaded[iframe.id])
-        {
-          var dataSrc = iframe.getAttribute('data-src');
+      if (entries[i].isIntersecting == true) {
+        if (!playersLoaded[iframe.id]) {
+          var dataSrc = iframe.getAttribute("data-src");
           iframe.src = dataSrc;
           playersLoaded[iframe.id] = true;
           console.log("creating " + iframe.id);
           players[iframe.id] = new YT.Player(iframe.id, {
             events: {
-                // call this function when player is ready to use
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
+              // call this function when player is ready to use
+              onReady: onPlayerReady,
+              onStateChange: onPlayerStateChange,
+            },
           });
-        }
-        else
-        {
+        } else {
           // console.log(players);
           var player = players[iframe.id];
           player.playVideo();
         }
-        console.log('Element has just become visible in screen: ' + iframe.id);
+        console.log("Element has just become visible in screen: " + iframe.id);
 
         //iframe.attr("src", iframe.data("src"));
-      }
-      else if(playersLoaded[iframe.id] == true)
-      {
+      } else if (playersLoaded[iframe.id] == true) {
         console.log("pausing " + iframe.id);
         console.log(players);
         // if(players[iframe.id].src == dataSrc)
@@ -72,12 +67,11 @@ console.log(entries);
         player.pauseVideo();
       }
     }
-    
-}, { threshold: [0.5] });
+  },
+  { threshold: [0.5] }
+);
 
-
-for(var i=0; i<videos.length; i++)
-{
+for (var i = 0; i < videos.length; i++) {
   playersLoaded[videos[i].id] = false;
   playersVisible[videos[i].id] = false;
   console.log("Observing " + videos[i].id);
@@ -86,26 +80,24 @@ for(var i=0; i<videos.length; i++)
 
 // this function gets called when API is ready to use
 function onYouTubePlayerAPIReady() {
-    // create the global player from the specific iframe (#video)
-    console.log("Youtube API ready");
+  // create the global player from the specific iframe (#video)
+  console.log("Youtube API ready");
 
-    for(var i=0; i<videos.length; i++)
-    {
-      var video = videos[i];
-      // if(video.id == null)
-        // continue;
-        console.log(videos[i].id);
-        if(!playersLoaded[video.id])
-          continue;
+  for (var i = 0; i < videos.length; i++) {
+    var video = videos[i];
+    // if(video.id == null)
+    // continue;
+    console.log(videos[i].id);
+    if (!playersLoaded[video.id]) continue;
 
-      players[video.id] = new YT.Player(video.id, {
-          events: {
-              // call this function when player is ready to use
-              'onReady': onPlayerReady,
-              'onStateChange': onPlayerStateChange
-          }
-      });
-    }
+    players[video.id] = new YT.Player(video.id, {
+      events: {
+        // call this function when player is ready to use
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  }
 
   console.log(players);
 }
@@ -115,13 +107,15 @@ function onPlayerStateChange(event) {
   console.log("visible: " + playersVisible[event.target.id] == true);
   console.log(event.target.h);
   console.log(playersVisible);
-  if (event.data != YT.PlayerState.PLAYING && playersVisible[event.target.h.id] == true) {
+  if (
+    event.data != YT.PlayerState.PLAYING &&
+    playersVisible[event.target.h.id] == true
+  ) {
     event.target.playVideo();
     event.target.setLoop(true);
   }
 }
 
-
 function onPlayerReady(event) {
-event.target.setLoop(true);
+  event.target.setLoop(true);
 }
